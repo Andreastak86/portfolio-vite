d36ec64f1cc6433db8ac358c433e3b9afe895c83
Andreastak86 <andreastak.kodehode@gmail.com>
2026-01-09 14:57:16 +0100

    add Pitch component and associated CSS file

---
+++ src/components/pitch/Pitch.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Pitch() {
+    return (
+        <div>
+            <p>Her må jeg huske å lage en selgene tekst om meg selv</p>
+        </div>
+    );
+}
